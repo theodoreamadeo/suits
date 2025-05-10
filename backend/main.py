4d5ba32929e4344a7466ca161270675fb2b42bc5
theodoreamadeo <THEO0013@e.ntu.edu.sg>
2025-05-10 09:36:26 +0800

    feat: added backend feature for skin tone analysis

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.routes.segmentation_routes import router as segmentation_router
+from api.routes.skin_tone_routes import router as skin_tone_router
 
-app = FastAPI(title="Face Segmentation API")
+app = FastAPI(title="Face Analysis API")
 
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Update this with your frontend URL
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -16,6 +17,7 @@ app.add_middleware(
 
 # Include routers
 app.include_router(segmentation_router, prefix="/api", tags=["Segmentation"])
+app.include_router(skin_tone_router, prefix="/api", tags=["Skin Tone"])
 
 if __name__ == "__main__":
     import uvicorn
