f4c5945b7f1f8c66efdc28ada1ae5750b7f92ee5
theodoreamadeo <THEO0013@e.ntu.edu.sg>
2025-05-10 09:37:27 +0800

    feat: updated api sevices for skin tone analysis

--- frontend/app/services/api.ts
+++ frontend/app/services/api.ts
@@ -25,3 +25,24 @@ export const processFrame = async (frameData: string): Promise<ProcessedFrame> =
         };
     }
 };
+
+export const analyzeSkinToneUpload = async (imageFile: File): Promise<any> => {
+  try {
+    const formData = new FormData();
+    formData.append ('image_file', imageFile);
+    formData.append ('image_type', 'color');
+
+    const response = await fetch (`http://localhost:8000/api/analyze-skin-tone`, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      throw new Error (`HTTP error! Status: ${response.status}`);
+    }
+    return await response.json ();
+  } catch (error) {
+    console.error ("Error analyzing skin tone:", error);
+    throw error;
+  }
+};
